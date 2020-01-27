28113441244b82bc1077443748aabfe4b382df32
cheng239 <59797330+cheng239@users.noreply.github.com>
2020-01-27 15:45:47 +0800

    Create 栈的应用：前缀表达式求值.cpp

---
+++ 栈的应用：前缀表达式求值.cpp
@@ -0,0 +1,116 @@
+#include <iostream>
+#define MAXSIZE 100
+
+using namespace std;
+
+int length;
+typedef struct Stack
+{
+	char *top;
+	char *base;
+	int StackSize;
+}SqStack;
+
+void CreateStack(SqStack &S);
+void Push(SqStack &S, char data);
+char Pop(SqStack &S);
+void InputArray(char Input[]);
+void fun(char Input[], SqStack &S);
+
+int main()
+{
+	SqStack S;
+	CreateStack(S);
+	char Input[MAXSIZE];
+	InputArray(Input);
+	fun(Input, S);
+	return 0;
+}
+
+void CreateStack(SqStack &S)
+{
+	S.base = new char[MAXSIZE];
+	S.top = S.base;
+	S.StackSize = MAXSIZE;
+}
+
+void Push(SqStack &S, char data)
+{
+	if (S.top - S.base == S.StackSize)
+		return;
+	else
+	{
+		*S.top = data;
+		S.top++;
+	}
+}
+
+char Pop(SqStack &S)
+{
+	if (S.top == S.base)
+		return -1;
+	else
+	{
+		--S.top;
+		return *S.top;
+	}
+}
+
+void Print(SqStack S)
+{
+	--S.top;
+	while (S.top != S.base - 1)
+	{
+		printf("%c -> ", *S.top);
+		S.top--;
+	}
+	printf("NULL\n");
+}
+
+void InputArray(char Input[])
+{
+	int i = 0;
+	char ch;
+	while (1)
+	{
+		cin >> ch;
+		char t = getchar();
+		Input[i] = ch;
+		i++;
+		if (t == '\n')
+			break;
+	}
+	length = i;
+}
+
+void fun(char Input[], SqStack &S)
+{
+	int i = -1;
+	char temp[3];
+	int a, b;
+	int result;
+	while (i != length)
+	{
+		do
+		{
+			i++;
+			Push(S, Input[length - i - 1]);
+		}while (Input[length - i - 1] >= '0' && Input[length - i - 1] <= '9');
+
+		for (int j = 0; j < 3; j++)
+		{
+			temp[j] = Pop(S);
+		}
+		a = (int)temp[1] - 48;
+		b = (int)temp[2] - 48;
+		switch (temp[0])
+		{
+		case '+':result = a + b; break;
+		case '-':result = a - b; break;
+		case '*':result = a * b; break;
+		case '/':result = a / b; break;
+		}
+		Push(S, (char)result+48);
+	}
+	printf("%.1f",(float)Pop(S)-48);
+}
